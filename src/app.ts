71074a73503ebc59bc8a9bf5d808537b4e951275
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-04-13 14:31:00 -0300

    chore: making env vars working with dot env

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
 import { env } from "./env";
--- src/env.ts
+++ src/env.ts
@@ -1,14 +1,17 @@
 import { z } from "zod";
 
 export const envSchema = z.object({
-    AWS_REGION: z.string().min(1),
-    AWS_ACCESS_KEY_ID: z.string().min(1),
-    AWS_SECRET_ACCESS_KEY: z.string().min(1),
-    DATABASE_URL: z.string().min(1).url(),
-    JWT_SECRET: z.string().min(1),
+   AWS_REGION: z.string().min(1),
+   AWS_ACCESS_KEY_ID: z.string().min(1),
+   AWS_SECRET_ACCESS_KEY: z.string().min(1),
+   DATABASE_URL: z.string().min(1),
+   JWT_SECRET: z.string().min(1),
 });
 
 export const env = envSchema.parse({
-    AWS_REGION: process.env.AWS_REGION,
+   AWS_REGION: process.env.AWS_REGION,
+   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+   DATABASE_URL: process.env.DATABASE_URL,
+   JWT_SECRET: process.env.JWT_SECRET,
 });
-
