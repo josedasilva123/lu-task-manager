a7f0607ad05535072e7e5349e09c55da3ab1085a
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-04-16 09:29:27 -0300

    chore: creating many is not going to be used

--- src/services/link/createMany.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "../../database/prisma";
-import { TLinkBody } from "../../schemas/link.schema";
-
-export const createMany = async (taskId: string, data: TLinkBody[]) => {
-    const linkCreateData = data.map(link => ({ ...link, taskId }));
-
-    await prisma.link.createMany({ data: linkCreateData });
-}
