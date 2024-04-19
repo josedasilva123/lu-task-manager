c75b734d8eaa26cc9a7a588f9a1781deb45fbc79
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-04-19 11:32:05 -0300

    feat: cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
-import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
+import cors from "@fastify/cors";
+import fastifyJwt from "@fastify/jwt";
 import { env } from "./env";
 import { errorHandler } from "./error/error";
 import { routes } from "./routes";
 
 export const app = fastify();
+
+app.register(cors);
 
 app.setErrorHandler(errorHandler);
 
