05cfd1ee50ec4b26a2b864a71dc017068abf73b1
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-04-24 13:59:02 -0300

    fix: fixing filters (were not working correctly)

--- src/services/task/getMany.ts
+++ src/services/task/getMany.ts
@@ -6,22 +6,26 @@ export const getMany = async (
    filters: IFilters,
    { skip, take }: IPagination = { skip: 0, take: 20 }
 ) => {
-   const count = await prisma.task.count({ where: { isDeleted: false }});
+   const count = await prisma.task.count({ where: { isDeleted: false } });
 
    const tasks = await prisma.task.findMany({
       where: {
-         categories: { some: { id: filters.categoryId } },
+         categories: filters.categoryId
+            ? { some: { id: filters.categoryId } }
+            : undefined,
          statusId: filters.statusId,
-         OR: [
-            { title: { contains: filters.search } },
-            { description: { contains: filters.search } },
-         ],
-         date: { lte: filters.date ? new Date(filters.date) : undefined },
+         date: filters.date ? { lte: new Date(filters.date) } : undefined,
+         OR: filters.search
+            ? [
+                 { title: { contains: filters.search } },
+                 { description: { contains: filters.search } },
+              ]
+            : undefined,
          isDeleted: false,
       },
       skip,
       take,
-      orderBy: { createdAt: "desc"},
+      orderBy: { createdAt: "desc" },
    });
 
    return { count, tasks };
